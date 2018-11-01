3ecc3149218ca6a9fd74f8fab95ca5abfebdc4bf
khj <kh@molamil.com>
2018-11-01 15:13:10 +0100

    add non composer plugin and pipelines

---
+++ web/app/mu-plugins/advanced-custom-fields-component_field/templates/admin/tools-migration.php
@@ -0,0 +1,6 @@
+
+<p><?php _e('Manually re-run the database migration. In some cases, the database update is incomplete or get interrupted when switching ACF Component Field plugin to new version. You can trigger the migration manully here.', 'acf-component_field'); ?></p>
+
+<p class="acf-submit">
+	<input type="submit" class="button button-primary" value="<?php _e('Re-run Migration', 'acf-component_field'); ?>" />
+</p>
